cdb2ac07cb0ffa959d596ef696770931586d79bc
liquanliang <liquanliang@phone580.com>
2017-08-11 18:29:09 +0800

    添加部分文件

---
+++ src/main/java/com/myprojct/ssm/bean/User.java
@@ -0,0 +1,40 @@
+package com.myprojct.ssm.bean;
+
+/**
+ * User表映射类
+ * @author fox
+ *
+ */
+public class User {
+
+    private Integer userId;  //用户ID
+    
+    private String userName;  //用户名
+    
+    private int userAge;     //用户年龄
+    
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+    
+}
